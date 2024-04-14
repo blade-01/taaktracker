c2d27c0c67e23514178c60e855befa51d6766967
Taofiq <animashauntaofiq@gmail.com>
2024-04-14 16:56:16 +0100

    feat: added index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
-const { t } = useI18n();
-const router = useRouter();
-const reRoute = (values: any): void => {
-  if (values.name) {
-    router.push(`/hi/${encodeURIComponent(values.name)}`);
-  }
-};
-
-useHead({
-  title: "Nuxt Boost",
+definePageMeta({
+  middleware: ["auth"],
 });
+const { user } = useAuth();
 </script>
 
 <template>
-  <NavigationHeader />
-  <Form
-    @submit="reRoute"
-    class="flex flex-col justify-center items-center gap-3 text-center"
-  >
-    <UiInputField
-      required
-      type="text"
-      name="name"
-      :placeholder="$t('placeholder')"
-      class="text-center mt-5"
-    />
-    <UiBtn :label="$t('go')" class="px-6 h-[35px]" />
-  </Form>
+  <div>Hello there</div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
