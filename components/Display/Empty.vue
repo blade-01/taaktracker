975fd867ae1a781eec674bf4239f3cfddb160772
Taofiq <animashauntaofiq@gmail.com>
2024-04-14 23:22:25 +0100

    feat: added 404 lottie

--- components/Display/Empty.vue
+++ components/Display/Empty.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div>
+    <client-only>
+      <Vue3Lottie
+        animationLink="https://lottie.host/7b015ac8-fd62-4709-84fb-aa78ff4de3fc/W2AiqnoXOU.json"
+        :height="200"
+        :width="300"
+      />
+    </client-only>
+  </div>
 </template>
 
 <style scoped></style>
