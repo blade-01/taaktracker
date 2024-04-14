e63414c0a0ffd3efe053ab0cbd86632a1d5638c5
Taofiq <animashauntaofiq@gmail.com>
2024-04-14 16:55:58 +0100

    feat: refactored default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nav = ref<boolean>(false);
+
+const toggleSidebar = () => {
+  nav.value = !nav.value;
+};
+
+provide("collapsible", {
+  nav,
+  toggleSidebar,
+});
+</script>
 
 <template>
-  <div class="flex flex-col justify-center items-center gap-5 text-center h-screen px-4">
-    <slot />
-    <NavigationFooter />
+  <div class="relative overflow-x-clip">
+    <NavigationSidebar :nav="nav" />
+    <main class="sidebar-wrapper" :class="{ 'mainbar-opened': nav }">
+      <div class="relative">
+        <NavigationTopbar />
+        <div class="p-4">
+          <NuxtPage />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.sidebar-wrapper {
+  @apply bg-bg-primary dark:bg-bg-secondary h-screen overflow-y-auto w-full transition-[margin-left] ease-out duration-500
+  /* MARGIN */
+  md:ml-[var(--sidebar-width-md)] lg:ml-[var(--sidebar-width-lg)] 2xl:ml-[var(--sidebar-width-2xl)]
+  /* WIDTH */
+  md:w-[calc(100%-var(--sidebar-width-md))] lg:w-[calc(100%-var(--sidebar-width-lg))] 2xl:w-[calc(100%-var(--sidebar-width-2xl))];
+}
+.mainbar-opened {
+  @apply ml-[var(--sidebar-width)] 
+  md:ml-0 md:w-full;
+}
+</style>
